1bc418211fb3d1fb7170572aabfad432c00b1556
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-04 21:50:41 +0200

    files updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import Painting from "./components/Painting";
+import PaintingList from "./components/PaintingList";
 import paintings from "./paintings.json";
 
 export default function App() {
   return (
     <div>
-      {paintings.map((painting) => (
-        <Painting
-          key={painting.id}
-          imageUrl={painting.url}
-          title={painting.title}
-          author={painting.author.tag}
-          profileUrl={painting.author.url}
-          price={painting.price}
-          quantity={paintings.quantity}
-        />
-      ))}
+      <PaintingList items={paintings} />
     </div>
   );
 }
