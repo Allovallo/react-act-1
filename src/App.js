3aed109ec2a4074ece61c040bdf418e50a6f8243
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-02-06 21:26:43 +0200

    files updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import PaintingList from "./components/PaintingList";
+import Section from "./components/Section";
 import paintings from "./paintings.json";
 
 export default function App() {
   return (
     <div>
-      <PaintingList items={paintings} />
+      <Section title="Топ тиждня">
+        <PaintingList items={paintings} />
+      </Section>
+      <Section />
     </div>
   );
 }
